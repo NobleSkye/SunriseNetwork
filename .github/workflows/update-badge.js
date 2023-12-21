7dfde4f5feab13cfd0d989ada3e85b0604f21f47
Skye <118846397+NobleSkye@users.noreply.github.com>
2023-12-20 21:20:29 -0800

    Create update-badge.js

---
+++ .github/workflows/update-badge.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+async function updateBadge() {
+  // Fetch player count from Minecraft server (replace with your server details)
+  const serverAddress = 'your.minecraft.server';
+  const serverPort = 25565;
+
+  const response = await fetch(`https://api.minetools.eu/ping/${serverAddress}/${serverPort}`);
+  const data = await response.json();
+  const playerCount = data.players.online;
+
+  // Create a dynamic badge URL
+  const badgeUrl = `https://img.shields.io/badge/Players%20Online-${playerCount}-brightgreen`;
+
+  // Write the badge URL to a file (badge.md)
+  fs.writeFileSync('badge.md', `[![Players Online](${badgeUrl})](https://your-server-url/)`);
+}
+
+updateBadge();
